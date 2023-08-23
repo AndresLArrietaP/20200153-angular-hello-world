1950535ee3c935d540d766989a15a082fd37301d
AndyArrieta <andrestransformers@hotmail.com>
2023-08-23 10:52:07 -0500

    1er avance (componentes)

---
+++ src/app/hello-world/hello-world.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hello-world',
+  template: `
+    <p>
+      hello-world works inline!
+    </p>
+    `
+  
+})
+export class HelloWorldComponent {
+  constructor() { }
+  
+  ngOnInit() {
+  }
+  
+}
